69cc3ea79859d4bfa78b22a68a5e885f7e30c1ef
Tao-Cheng <tao.cheng.cs@gmail.com>
2019-12-24 18:03:20 +0800

    add search function

--- be/model/searcher.py
+++ be/model/searcher.py
@@ -1,5 +1,10 @@
 """实现搜索"""
+import sqlite3 as sqlite
+from be.model import error
 from be.model import db_conn
+import psycopg2
+from be.model.constants import Constants as C
+import json
 
 
 class Searcher(db_conn.DBConn):
@@ -8,7 +13,7 @@ class Searcher(db_conn.DBConn):
     def __init__(self):
         db_conn.DBConn.__init__(self)
 
-    def search(self,content_scope,book_scope,max_id,keyword,):
+    def search(self,content_scope:str,book_scope:str,max_id:int,keyword:str):
         """查找内容与关键字匹配的书籍
 
         Args:
@@ -21,3 +26,27 @@ class Searcher(db_conn.DBConn):
             code,bookinfo json str
 
         """
+
+        if content_scope=="tags":
+            search_feild="search_content1"
+        elif content_scope=="titles":
+            search_feild="search_content2"
+        else:
+            return 400,"No such feild"
+        
+        if book_scope=="all":
+            search_scope = 1
+        else:
+            if not self.store_id_exist(book_scope):
+                return error.error_non_exist_store_id(book_scope)
+            search_scope=0
+        
+        if search_scope:
+            sql= "select * from store where %s@@to_tsquery('%s') limit %d"%(content_scope, keyword,keyword)
+        else:
+            sql= "select * from store where store_id=%s and %s@@to_tsquery('%s') limit %d"%(search_scope,search_feild, keyword,keyword)
+        cursor=self.conn.cursor()
+        cursor.execute(sql)
+        self.conn.commit()
+        result = cursor.fetchall()        
+        return 200,json.dumps(result)
